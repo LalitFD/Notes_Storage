2311177a8bb2f1de6e947f2667fbd32c7c604953
Lalit doriya <lalit211035itt@sdbc.ac.in>
2025-06-26 21:24:21 +0530

    new changes

--- config/db.js
+++ config/db.js
@@ -3,12 +3,29 @@ import mysql from 'mysql2/promise';
 import dotenv from "dotenv"
 dotenv.config();
 
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  acquireTimeout: 60000,  // 60 seconds
+  timeout: 60000,
+  reconnect: true,
+  connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 async function testConnection() {
   try {
